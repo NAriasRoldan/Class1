6c43452eb0c52edf4971e47eef1a6eded639c690
Natalia Arias Roldan <nataliaarias523@gmail.com>
2023-02-25 07:43:39 -0500

    Desarrollo de clase

---
+++ MyFirstOOPCode-Saturday/MyFirstOOPCode-Saturday/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyFirstOOPCode_Saturday
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var dateObjectBirthDate = new Date(1920, 2, 8);
+                var dateObjectHiringDate = new Date(2023, 1, 1);
+
+                Console.WriteLine("");
+
+                Employee salaryEmployeeObject = new SalaryEmployee()
+                {
+                    Id= 104580581,
+                    FirstName = "Maria",
+                    LastName = "Sandoval",
+                    BirthDate = dateObjectBirthDate,
+                    HiringDate = dateObjectHiringDate,
+                    IsActive = true,
+                    Salary = 1600000.56M
+                };
+
+                Console.WriteLine(salaryEmployeeObject.ToString());
+
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
