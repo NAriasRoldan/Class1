07026cba1ff0b0f11e42347caecdb9e55c9f032a
Natalia Arias Roldan <nataliaarias523@gmail.com>
2023-02-25 10:01:34 -0500

    Exercise solution

--- MyFirstOOPCode-Saturday/MyFirstOOPCode-Saturday/Program.cs
+++ MyFirstOOPCode-Saturday/MyFirstOOPCode-Saturday/Program.cs
@@ -1,37 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyFirstOOPCode_Saturday
 {
     public class Program
     {
+        public static List<string> OptionsMenu = new List<string>{ "1. Show employees.", "2. Add new employee.", "3. End program. \n" };
         static void Main(string[] args)
         {
-            try
-            {
-                var dateObjectBirthDate = new Date(1920, 2, 8);
-                var dateObjectHiringDate = new Date(2023, 1, 1);
+            int  OptionSelected = 0;
+            List<SalaryEmployee> Employees = new List<SalaryEmployee>();
+            do {
+                Utils.Show(OptionsMenu);
+                OptionSelected = Utils.GetValueTypeInt("option");
 
-                Console.WriteLine("");
-
-                Employee salaryEmployeeObject = new SalaryEmployee()
+                switch (OptionSelected)
                 {
-                    Id= 104580581,
-                    FirstName = "Maria",
-                    LastName = "Sandoval",
-                    BirthDate = dateObjectBirthDate,
-                    HiringDate = dateObjectHiringDate,
-                    IsActive = true,
-                    Salary = 1600000.56M
-                };
-
-                Console.WriteLine(salaryEmployeeObject.ToString());
-
-                Console.WriteLine();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+                    case 1:
+                        Utils.Show(Employees);
+                        break;
+                    case 2:
+                        Employees.Add(new SalaryEmployee()
+                        {
+                            Id = Employees.Count + 1,
+                            FirstName = Utils.GetValueTypeString("firstName"),
+                            LastName = Utils.GetValueTypeString("lastName"),
+                            BirthDate = new Date(Utils.GetValueTypeInt("Year of birthDate"), Utils.GetValueTypeInt("month of birthDate"), Utils.GetValueTypeInt("day of birthDate")),
+                            HiringDate = new Date(Utils.GetValueTypeInt("Year of Hiring"), Utils.GetValueTypeInt("month of Hiring"), Utils.GetValueTypeInt("day of Hiring")),
+                            IsActive = Utils.GetValueTypeBoolean("is Active"),
+                            Salary = Utils.GetValueTypeDecimal("Salary")
+                        });
+                        Console.WriteLine("Successful registration");
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        Console.WriteLine("The option selected is not defined");
+                        break;
+                }
+            } while (OptionSelected != 3);
         }
+
     }
 }
---
+++ MyFirstOOPCode-Saturday/MyFirstOOPCode-Saturday/Utils.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstOOPCode_Saturday
+{
+    internal static class Utils
+    {
+        public static void Show<T>(List<T> Options) {
+            Console.WriteLine("\n");
+            foreach (T option in Options)
+            {
+                Console.WriteLine(option.ToString());
+            }
+        }
+
+        public static int GetValueTypeInt(string OptionEnter) {
+            try
+            {
+                Console.WriteLine($"\n Enter the {OptionEnter}");
+                return  Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("\n The value entered is not valid \n");
+                return GetValueTypeInt(OptionEnter);
+            }
+        }
+
+        public static string GetValueTypeString(string OptionEnter)
+        {
+            try
+            {
+                Console.WriteLine($" \n Enter the {OptionEnter}");
+                return Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("The value entered is not valid");
+                return GetValueTypeString(OptionEnter);
+            }
+        }
+
+        public static bool GetValueTypeBoolean(string OptionEnter)
+        {
+            try
+            {   
+                Console.WriteLine($" \n Enter the {OptionEnter}");
+                Console.WriteLine("\"true\" : is active\r\n\"false\": is inactive");
+                return Convert.ToBoolean(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("\n The value entered is not valid \n");
+                return GetValueTypeBoolean(OptionEnter);
+            }
+        }
+
+        public static decimal GetValueTypeDecimal(string OptionEnter)
+        {
+            try
+            {
+                Console.WriteLine($" \n Enter the {OptionEnter}");
+                return Convert.ToDecimal(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("\n The value entered is not valid \n");
+                return GetValueTypeDecimal(OptionEnter);
+            }
+        }
+    }
+}
